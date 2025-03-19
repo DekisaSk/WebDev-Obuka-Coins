5e3d6a3e49fe074f99e26b98b638bb78df7faac9
unknown <skiljic.dejan@gmail.com>
2025-03-19 13:27:26 +0100

    Zadatak 2 - part 2/2 - Finished

--- Zadatak 2/script.js
+++ Zadatak 2/script.js
@@ -1,20 +1,56 @@
-document.getElementById("searchBar").addEventListener("input", function () {
-  let searchItem = this.textContent.toLowerCase();
-  let items = document.getElementsByClassName("listItem").textContent;
+document.getElementById("searchBar").addEventListener("keypress", function (e) {
+  let searchItem = this.value.toLowerCase();
 
-  for (let i = 0; i < items.lenght; i++) {
-    // filter - hide uncsearched
+  if (e.key !== "Enter") {
+    return;
+  }
+
+  let items = Array.from(
+    document.getElementById("itemList").getElementsByTagName("li")
+  );
+
+  if (!isNullOrEmpty(searchItem)) {
+    items.forEach((item) => {
+      let p = item.getElementsByTagName("p")[0];
+      let itemName = p.textContent.toLowerCase();
+      if (itemName !== searchItem) {
+        item.style.display = "none";
+      } else {
+        item.style.display = "flex";
+      }
+    });
+  } else {
+    items.forEach((item) => {
+      item.style.display = "flex";
+    });
   }
 });
 
 document.getElementById("buttonSubmit").addEventListener("click", function () {
-  let newItem = document.getElementById("textBoxNewItem").textContent;
+  let newItem = document.getElementById("textBoxNewItem").value;
+  createLiElement(newItem);
+});
 
-  if (!newItem) {
-    //add new item
+document.getElementById("itemList").addEventListener("click", function (event) {
+  if (event.target && event.target.classList.contains("buttonDelete")) {
+    event.target.closest("li").remove();
   }
 });
 
-document.getElementById("buttonDelete").addEventListener("click", function () {
-  this.parentElement.remove();
-});
+function isNullOrEmpty(str) {
+  return str == null || str.trim() == "";
+}
+
+function createLiElement(elementName) {
+  console.log(elementName);
+  if (isNullOrEmpty(elementName)) {
+    console.log("Element is empty");
+    return;
+  }
+
+  let ul = document.getElementsByTagName("ul")[0];
+  const li = document.createElement("li");
+
+  li.innerHTML = `<p>${elementName}</p> <button class="buttonDelete">x</button>`;
+  ul.append(li);
+}
